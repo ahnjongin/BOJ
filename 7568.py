892f497a280d907ed271ce99d80e80ae2f3914d8
ahnjongin <goodtime1998@naver.com>
2021-07-28 11:54:56 +0900

    brute force

---
+++ 7568.py
@@ -0,0 +1,10 @@
+n=int(input())
+student=[]
+for _ in range(n):
+    student.append(list(map(int, input().split())))
+for i in student:
+    rank=1
+    for j in student:
+        if i[0]<j[0] and i[1]<j[1]:
+            rank+=1
+    print(rank, end=" ")
